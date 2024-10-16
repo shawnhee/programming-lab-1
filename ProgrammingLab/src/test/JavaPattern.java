eadcf0d28dcafcc3b2513e237432f146cb7c90ef
shawnhee <shawnhee2722@gmail.com>
2024-10-16 11:09:51 +0800

    Complete Lab 1 Contents

---
+++ ProgrammingLab/src/test/JavaPattern.java
@@ -0,0 +1,10 @@
+package test;
+
+public class JavaPattern {
+	public static void main(String[]args) {
+		System.out.println("       J");
+		System.out.println("J aaa v vaaa");
+		System.out.println("J J aa vv a a");
+		System.out.println("J J aa vv a a");
+	}
+}
---
+++ ProgrammingLab/src/test/LoveJava.java
@@ -0,0 +1,11 @@
+package test;
+
+public class LoveJava {
+	public static void main(String[]args) {
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+	}
+}
